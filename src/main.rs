e995bb568f12816bb562280ecabf91c1bb92b8f6
EthanDGee <workethangee@gmail.com>
2026-02-27 23:50:40 -0700

    feat: added basic coommand line argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-use std::string;
-use tree_sitter::{Node, Parser};
+use std::fs;
+use tree_sitter::Parser as TreeSitterParser;
 mod diagram;
-use diagram::Diagram;
+
+enum Language {
+    Rust,
+    Cpp,
+    Java,
+}
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Programming language of the source file
+    #[arg(short, long)]
+    lang: String,
+    /// Path to the source file
+    #[arg(short, long)]
+    file_path: String,
+}
 
 fn main() {
+    let args = Args::parse();
     // 1. Create the parser and set language
-    let mut parser = Parser::new();
-
+    let mut parser = TreeSitterParser::new();
     parser
         .set_language(&tree_sitter_rust::LANGUAGE.into())
         .expect("Error loading Rust grammar");
 
-    // Parse souce code
-    let source_code = "fn main() { let x = 1; }";
-    let tree = parser.parse(source_code, None).unwrap();
+    // Parse source code
+    let source_code = fs::read_to_string(&args.file_path).expect("Failed to read file");
+    let tree = parser.parse(&source_code, None).unwrap();
 
     // 4. Get the root node
     let root_node = tree.root_node();
