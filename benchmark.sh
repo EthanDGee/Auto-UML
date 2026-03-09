492943c7ca604a078617d3563dfe9bf8a10f3dc4
EthanDGee <workethangee@gmail.com>
2026-03-09 13:12:15 -0600

    feat: Expanded benchmark testing
    Now included benchmarks for all supported languages, as well as speed
    ups through shallow cloning. Also refactored for better readability.

--- benchmark.sh
+++ benchmark.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+RUNS=100
+WARMUP=3
+
 echo "=== Building Latest Release ==="
 cargo build --release
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BENCHMARK_DIR="$SCRIPT_DIR/benchmarks"
+TOOL_PATH="target/release/auto-uml"
 
 mkdir -p "$BENCHMARK_DIR"
 cd "$BENCHMARK_DIR"
@@ -14,11 +18,39 @@ cd "$BENCHMARK_DIR"
 echo "=== Cloning codebases ==="
 
 if [ ! -d "coreutils" ]; then
-  git clone https://github.com/uutils/coreutils.git
+  git clone --depth 1 https://github.com/uutils/coreutils.git
 fi
 
 if [ ! -d "Chart.js" ]; then
-  git clone https://github.com/chartjs/Chart.js
+  git clone --depth 1 https://github.com/chartjs/Chart.js
+fi
+
+if [ ! -d "BuildCLI" ]; then
+  git clone --depth 1 https://github.com/BuildCLI/BuildCLI
+fi
+
+if [ ! -d "faker-cxx" ]; then
+  git clone --depth 1 https://github.com/cieslarmichal/faker-cxx
+fi
+
+if [ ! -d "ReactiveUI" ]; then
+  git clone --depth 1 https://github.com/reactiveui/ReactiveUI
+fi
+
+if [ ! -d "authass" ]; then
+  git clone --depth 1 https://github.com/authpass/authpass.git
+fi
+
+if [ ! -d "jupyterlab" ]; then
+  git clone --depth 1 ttps://github.com/authpass/authpass.git
+fi
+
+if [ ! -d "bitwarden-server"]; then
+  git clone --depth 1 https://github.com/bitwarden/server bitwarden-server
+fi
+
+if [! -d "Platypus"]; then
+  git clone --depth 1 https://github.com/sveinbjornt/Platypus
 fi
 
 cd "$SCRIPT_DIR"
@@ -26,15 +58,36 @@ cd "$SCRIPT_DIR"
 echo "Running benchmarks..."
 
 echo "=== Benchmark: This Codebase ==="
-hyperfine --runs 100 --warmup 3 'target/release/auto-uml --source-code src/ --lang rust --destination uml.md'
-
-echo "=== Benchmark: CoreUtils ==="
-hyperfine --runs 100 --warmup 3 "target/release/auto-uml --source-code $BENCHMARK_DIR/coreutils --lang rust --destination uml.md"
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code src/ --lang rust --destination uml.md"
 
 echo "=== Benchmark: Chart.js ==="
-hyperfine --runs 100 --warmup 3 "target/release/auto-uml --source-code $BENCHMARK_DIR/Chart.js --lang javascript --destination uml.md"
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/Chart.js --lang javascript --destination uml.md"
+
+echo "=== Benchmark: CoreUtils ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/coreutils --lang rust --destination uml.md"
+
+echo "=== Benchmark: BuildCLI ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/BuildCLI --lang java --destination uml.md"
+
+echo "=== Benchmark: faker-cxx ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/faker-cxx --lang cpp --destination uml.md"
+
+echo "=== Benchmark: AuthPass ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/authpass --lang dart --destination uml.md"
+
+echo "=== Benchmark: JupyterLab ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/jupyterlab --lang typescript --destination uml.md"
+
+echo "=== Benchmark: bitwarden-server ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/bitwarden-server --lang csharp --destination uml.md"
+
+echo "=== Benchmark: Platypus ==="
+hyperfine --runs $RUNS --warmup $WARMUP "$TOOL_PATH --source-code $BENCHMARK_DIR/Platypus --lang objective-c --destination uml.md"
+
+echo ""
 
 echo "Cleaning up benchmarks..."
 rm -rf "$BENCHMARK_DIR"
+rm uml.md
 
 echo "Done!"
